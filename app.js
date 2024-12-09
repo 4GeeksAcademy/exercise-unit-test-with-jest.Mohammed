16d4a75ef03c6eba7fdd8c2ffb4acdb05490774f
Mohammed El Mangadi <138013464+Riyadaram@users.noreply.github.com>
2024-12-09 09:50:14 +0000

    First Commit

---
+++ app.js
@@ -0,0 +1,21 @@
+let oneEuroIs = {
+    "JPY": 156.5,
+    "USD": 1.07,
+    "GBP": 0.87,
+};
+
+function fromDollarToYen(dollars) {
+    return (dollars / oneEuroIs["USD"]) * oneEuroIs["JPY"];
+}
+
+function fromEuroToDollar(euros) {
+    return euros * oneEuroIs["USD"];
+}
+
+function fromYenToPound(yen) {
+    return (yen / oneEuroIs["JPY"]) * oneEuroIs["GBP"];
+}
+
+module.exports = { fromEuroToDollar, fromDollarToYen, fromYenToPound };
+
+console.log(fromDollarToYen(10))
